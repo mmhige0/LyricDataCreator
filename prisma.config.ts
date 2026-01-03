ffeadb9bdd394784414bf76f6a42091e468f4647
mmhige <mm.hige.0@gmail.com>
2026-01-03 17:41:38 +0900

    Revert "fix: Allow prisma config without env"
    This reverts commit 3d709d78731b6f3363a90aebe16bcebb7ac13a0f.

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,12 +1,10 @@
-import { defineConfig } from '@prisma/config'
+import { defineConfig, env } from '@prisma/config'
 
 export default defineConfig({
   schema: 'prisma/schema.prisma',
-  datasource: process.env.DATABASE_URL
-    ? {
-        url: process.env.DATABASE_URL,
-      }
-    : undefined,
+  datasource: {
+    url: env('DATABASE_URL'),
+  },
   migrations: {
     path: 'prisma/migrations',
   },
